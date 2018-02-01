200677d500b8b4cf638f1250533aa926c27df56c
AlekseyS23 <34762840+AlekseyS23@users.noreply.github.com>
2018-02-01 17:40:51 +0200

    second commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aleksey
+ * Date: 20.01.2018
+ * Time: 18:06
+ */
+
+
+$array = array(12, 23, 3, 14, 5git 5);
+echo count($array)."<br/>";
+sort ($array);
+print_r($array);
+echo "<br/>";
